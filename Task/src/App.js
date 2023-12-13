9b90f2d42a2431ab8282d53ff4c353bd74f1a58a
itc-s22028 <s22028@std.it-college.ac.jp>
2023-12-13 12:31:40 +0900

    検索完成

--- Task/src/App.js
+++ Task/src/App.js
@@ -1,34 +1,3 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-// import Home from './HomeScene/Home';
-// import Search from './SearchScene/Search';
-// import Intro from './IntroScene/Intro';
-// import Login from './components/RegisterForm.js'
-
-
-// function App() {
-//   return (
-//     <Router>
-//       <div>
-//         <hr />
-
-//         <Routes>
-//           <Route path="/" element={<Login />} />
-//           <Route path="/home" element={<Home />} />
-//           <Route path="/Search" element={<Search />} />
-//           <Route path='/Intro' element={<Intro />} />
-//         </Routes>
-        
-        
-        
-
-//       </div>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './HomeScene/Home';
